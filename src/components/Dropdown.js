abe60521c586e54dc7214868ebbc3cbbc8ed2633
Ekaterina Shchegoleva <kateg@list.ru>
2022-06-28 20:03:53 +0300

    added some beauty

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,48 +4,53 @@ import { useDispatch, useSelector } from "react-redux";
 import { ADD_CATEGORY } from '../redux/actions/actions';
 import { categorySelector } from '../redux/categoryReducer/selector';
 
-function Dropdown({selected, setSelected})  {
+
+function Dropdown({ selected, setSelected }) {
     const [isActive, setIsActive] = useState(false);
     const [addActive, setAddActive] = useState(false);
-    const [newCategory, setNewCategory] = useState('');  
-    const categoryArr = useSelector(categorySelector); 
+    const [newCategory, setNewCategory] = useState('');
+    const categoryArr = useSelector(categorySelector);
     const dispatch = useDispatch();
 
-    const addCategory = (e) => {        
-        e.preventDefault() 
-        const cat= {
-            id: Date.now(), 
-            name: newCategory
+    const addCategory = (e) => {
+        e.preventDefault()
+        if (newCategory !== '') {
+            const cat = {
+                id: Date.now(),
+                name: newCategory
+            }
+            dispatch({ type: ADD_CATEGORY, payload: cat })
+            setNewCategory('');
+
+            console.log(e.target)
         }
-        dispatch({type: ADD_CATEGORY, payload: cat})
-        setNewCategory('');  
-        
-        console.log(e.target)
+
     }
 
     return (
+
         <div className='dropdown'>
             <div className='dropdown-btn'
-            onClick={(e) => setIsActive(!isActive)}>
-               {selected}
+                onClick={(e) => setIsActive(!isActive)}>
+                {selected}
             </div>
             {isActive && (
-            <div className='dropdown-content'>
-                {categoryArr.map((cat) => (
-                    <div key={cat.id} onClick={(e) => {setSelected(cat.name); setIsActive(false);}}
-                     >
-                      <span> {cat.name}</span> 
+                <div className='dropdown-content'>
+                    {categoryArr.map((cat) => (
+                        <div key={cat.id} onClick={(e) => { setSelected(cat.name); setIsActive(false); }}
+                        >
+                            <span> {cat.name}</span>
+                        </div>
+                    ))}
+                    <div onClick={(e) => setAddActive(!addActive)} className='dropdown-content-plus'>
                     </div>
-                ))}
-                <div onClick={(e) => setAddActive(!addActive)} className='dropdown-content-plus'>
-                    </div>
-                {addActive && (
-                <div className='dropdown-content-plus-form'>
-                <input   placeholder='Add category' onChange={(e) => setNewCategory(e.target.value)} /> 
-            <button onClick={addCategory} >Add new category</button>
+                    {addActive && (
+                        <div className='dropdown-content-plus-form'>
+                            <input placeholder='Add category' onChange={(e) => setNewCategory(e.target.value)} />
+                            <button onClick={addCategory} >Add new category</button>
+                        </div>
+                    )}
                 </div>
-                )}
-            </div>
             )}
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import './Header.scss'
 import React from 'react';
-import {NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
-        <header>
-            <div>
-          <NavLink to='/about'>About</NavLink>
-          <NavLink to='/task'>Tasks</NavLink>
-          </div>
-        </header>
-        <main>     
-            <Outlet />     
-        </main>
-        <footer>
-            
-        </footer>
+            <header>
+                <div>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/task'>Tasks</NavLink>
+                </div>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+
+            </footer>
         </>
     );
 };
